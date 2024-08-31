988108200f4f30561ef81be19f333706c42d72d5
4bdkdrrr <ided.263@gmail.com>
2024-09-01 03:06:57 +0900

    fetch api

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import AnimeList from "./components/AnimeList"
-const Home = () => {
+const Home = async () => {
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+  const anime = await response.json()
+  console.log(anime)
+  
+
   return (
       <div>
           <h1>Paling Populer</h1>
