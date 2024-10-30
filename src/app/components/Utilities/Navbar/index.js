0e7e5d1488e5dfa0fd24755e46160d42620893ba
cahyadewi11 <cahyadewimarif@gmail.com>
2024-10-30 11:45:22 +0900

    add navbar

--- src/app/components/Utilities/Navbar/index.js
+++ src/app/components/Utilities/Navbar/index.js
@@ -1,10 +1,11 @@
-import { headers } from "next/headers"
+import Link from "next/link"
 
 const Navbar = () => {
     return (
         <header className="bg-red-500">
-            <div>
-                <Link href="/">CuyAnimeList</Link>
+            <div className="flex justify-between p-4">
+                <Link href="/" className="font-bold text-white text-2xl">CuyAnimeList</Link>
+                <input placeholder="cari anime....." className="text-center"/>
             </div>
         </header>
     )
