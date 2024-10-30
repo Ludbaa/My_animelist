d58bdfacf4abbe82e3ecbe1fa168d5957701c187
cahyadewi11 <cahyadewimarif@gmail.com>
2024-10-31 02:20:28 +0900

    add responsive

--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ const Home = async () => {
   
   return (
     <div>
-      <div className="flex justify-between p-3">
+      <div className="flex justify-between p-3 items-center">
         <h1 className="text-2xl font-bold">Paling Populer</h1>
-        <Link href="/populer" className="text-xl underline hover:text-red-500 transition-all">Lihat Semua</Link>
+        <Link href="/populer" className="md:text-xl text-md underline hover:text-red-500 transition-all">Lihat Semua</Link>
       </div>
       <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
       {anime.data.map(data => {
