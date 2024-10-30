1902cbce00aef7a7718030038aea28fbb4c26a88
cahyadewi11 <cahyadewimarif@gmail.com>
2024-10-30 11:58:12 +0900

    limit data

--- src/app/components/Utilities/Navbar/index.js
+++ src/app/components/Utilities/Navbar/index.js
@@ -3,9 +3,9 @@ import Link from "next/link"
 const Navbar = () => {
     return (
         <header className="bg-red-500">
-            <div className="flex justify-between p-4">
+            <div className="flex md:flex-row flex-col justify-between p-4 gap-2">
                 <Link href="/" className="font-bold text-white text-2xl">CuyAnimeList</Link>
-                <input placeholder="cari anime....." className="text-center"/>
+                <input placeholder="cari anime....." className=""/>
             </div>
         </header>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import AnimeList from "./components/AnimeList"
 const Home = async () => {
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
   const anime = await response.json()
   console.log(anime)
   
   return (
-      <div>
-      <h1>Paling Populer</h1>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+    <div>
+      <div className="p-3">
+      <h1 className="text-2xl">Paling Populer</h1>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
       {anime.data.map(data => {
         return (
         <div key={data.mal_id} className="shadow-xl">
